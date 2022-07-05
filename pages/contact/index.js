bd33eaf78fc13aa98f14c1d523f3393fdb65dbd2
Raison Joseph <raisonjoseph@MacBook-Pro.local>
2022-07-05 15:20:49 +0530

    Add contact page and style fixes

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,29 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../components/Button";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [phoneOrEmail, setPhoneOrEmail] = useState("");
+  const [project, setProject] = useState("Website");
+  const [projectDescription, setProjectDescription] = useState("");
+
+  const handleOnNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleOnPhoneOrEmailChange = (e) => {
+    setPhoneOrEmail(e.target.value);
+  };
+
+  const handleRadioButtonClick = (e) => {
+    setProject(e.target.value);
+  };
+
+  const handleProjectDescriptionChange = (e) => {
+    setProjectDescription(e.target.value);
+  };
+
   return (
     <React.Fragment>
       <Head>
@@ -11,14 +33,14 @@ const Contact = () => {
       <section className="h-100">
         <div className="row h-100">
           <div className="col-sm-5">
-            <div className="section">
+            <div className="section lets-chat">
               <div className="content">
-                <h2>I observe things, Occasionally I connect them ✨</h2>
-                <div className="reels">
-                  <div className="reel-item"></div>
-                  <div className="reel-item"></div>
-                  <div className="reel-item"></div>
-                </div>
+                <h2>Let’s Chat! 💬</h2>
+                <p>
+                  For quick replies call me (📲 9946701501). For easy connect,
+                  find me on instagram or you can also email me at
+                  arun14949@gmail.com
+                </p>
               </div>
               <div className="footer">
                 <p>© {new Date().getFullYear()} Inspired Monster · Credits</p>
@@ -26,7 +48,147 @@ const Contact = () => {
             </div>
           </div>
           <div className="col-sm-7">
-            <div className="section"></div>
+            <div className="section contact">
+              <div className="content">
+                <h3>Have a project to talk about?</h3>
+                <div className="row gutter-3">
+                  <div className="col-sm">
+                    <div className="form-group mb-sm-3">
+                      <input
+                        type="text"
+                        id="floating_name"
+                        onChange={handleOnNameChange}
+                        value={name}
+                        required
+                      />
+                      <label className="floating-label">Your Name</label>
+                    </div>
+                  </div>
+                  <div className="col-sm">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="floating_name"
+                        onChange={handleOnPhoneOrEmailChange}
+                        value={phoneOrEmail}
+                        required
+                      />
+                      <label className="floating-label">Email or Phone *</label>
+                    </div>
+                  </div>
+                </div>
+                <h3 className="mt-5">What kind of project is this?</h3>
+                <div className="projects">
+                  <div className="project-item">
+                    <input
+                      id="website"
+                      className="radio-button"
+                      type="radio"
+                      name="projects"
+                      value="Website"
+                      onChange={handleRadioButtonClick}
+                      checked={project === "Website"}
+                    />
+                    <div className="radio-tile">
+                      <label htmlFor="website">Website</label>
+                    </div>
+                  </div>
+                  <div className="project-item">
+                    <input
+                      type="radio"
+                      id="branding"
+                      name="projects"
+                      value="Branding"
+                      className="radio-button"
+                      onChange={handleRadioButtonClick}
+                      checked={project === "Branding"}
+                    />
+                    <div className="radio-tile">
+                      <label htmlFor="branding">Branding</label>
+                    </div>
+                  </div>
+                  <div className="project-item">
+                    <input
+                      type="radio"
+                      id="app"
+                      name="projects"
+                      value="App / Software"
+                      className="radio-button"
+                      onChange={handleRadioButtonClick}
+                      checked={project === "App / Software"}
+                    />
+
+                    <div className="radio-tile">
+                      <label htmlFor="app">App / Software</label>
+                    </div>
+                  </div>
+                  <div className="project-item">
+                    <input
+                      type="radio"
+                      id="graphic-design"
+                      name="projects"
+                      value="Graphic Design"
+                      className="radio-button"
+                      onChange={handleRadioButtonClick}
+                      checked={project === "Graphic Design"}
+                    />
+
+                    <div className="radio-tile">
+                      <label htmlFor="graphic-design">Graphic Design</label>
+                    </div>
+                  </div>
+                  <div className="project-item">
+                    <input
+                      type="radio"
+                      id="consulting"
+                      name="projects"
+                      value="Consulting"
+                      className="radio-button"
+                      onChange={handleRadioButtonClick}
+                      checked={project === "Consulting"}
+                    />
+
+                    <div className="radio-tile">
+                      <label htmlFor="consulting">Consulting</label>
+                    </div>
+                  </div>
+                  <div className="project-item">
+                    <input
+                      type="radio"
+                      id="other"
+                      name="projects"
+                      value="Other"
+                      className="radio-button"
+                      onChange={handleRadioButtonClick}
+                      checked={project === "Other"}
+                    />
+
+                    <div className="radio-tile">
+                      <label htmlFor="other">Other</label>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group mt-4">
+                  <textarea
+                    required
+                    rows={4}
+                    onChange={handleProjectDescriptionChange}
+                    value={projectDescription}
+                  />
+                  <label className="floating-label">
+                    Describe your project *
+                  </label>
+                </div>
+                <Button
+                  className="mt-4"
+                  disabled={
+                    !name || !phoneOrEmail || !project || !projectDescription
+                  }
+                >
+                  Submit Project
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </section>
